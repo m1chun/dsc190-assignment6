b7845759cc09c046aabda93f274bb21fc8f2f0b9
Mieko Chun <165838850+m1chun@users.noreply.github.com>
2026-05-10 17:15:37 -0700

    Add tests

---
+++ src/dsc190_assignment6/nldate.py
@@ -0,0 +1,3 @@
+from datetime import date
+def parse(s: str, today: date | None = None) -> date:
+    
---
+++ tests/test_nl1date.py
@@ -0,0 +1,44 @@
+from datetime import date
+from src.dsc190_assignment6.nldate import parse
+
+TODAY = date(2025, 1, 1)
+
+
+def test_today():
+    assert parse("today", TODAY) == TODAY
+
+
+def test_tomorrow():
+    assert parse("tomorrow", TODAY) == date(2025, 1, 2)
+
+
+def test_yesterday():
+    assert parse("yesterday", TODAY) == date(2024, 12, 31)
+
+
+def test_5_days_after():
+    assert parse("5 days after today", TODAY) == date(2025, 1, 6)
+
+
+def test_3_days_before():
+    assert parse("3 days before today", TODAY) == date(2024, 12, 29)
+
+
+def test_2_weeks_after():
+    assert parse("2 weeks after today", TODAY) == date(2025, 1, 15)
+
+
+def test_1_year_after():
+    assert parse("1 year after today", TODAY) == date(2026, 1, 1)
+
+
+def test_months_before():
+    assert parse("2 months before today", TODAY) == date(2024, 11, 1)
+
+
+def test_absolute_date():
+    assert parse("December 1st, 2025", TODAY) == date(2025, 12, 1)
+
+
+def test_next_tuesday():
+    assert parse("next Tuesday", date(2025, 1, 1)) == date(2025, 1, 7)
